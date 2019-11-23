d456508fdbe840a6037874047f58b1f0c806787e
Alex.Prokofev <thetroller186@gmail.com>
2019-11-23 17:01:58 +0500

    123

---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main">
+    <div class="map"><img @click="click" src="" alt="Карта" /></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Map",
+  data() {
+    return {};
+  },
+  methods: {
+    click(e) {
+        
+    }
+  }
+};
+</script>
+
+<style scoped>
+.ymap-container {
+  height: 720px;
+}
+.red {
+  color: red;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,18 +2,25 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Payment from '@/components/Payment'
 import Index from '@/components/Index'
+import MapMain from '@/components/Map'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
     mode: "history",
     routes: [{
-        path: "/",
-        name: "Index",
-        component: Index
-    }, {
-        path: '/payment',
-        component: Payment,
-        name: 'Payment'
-    }]
+            path: "/",
+            name: "Index",
+            component: Index
+        }, {
+            path: '/payment',
+            component: Payment,
+            name: 'Payment'
+        },
+        {
+            path: '/map',
+            component: MapMain,
+            name: 'MapMain'
+        }
+    ]
 })
